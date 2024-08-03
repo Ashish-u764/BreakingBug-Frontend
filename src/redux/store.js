64428669487758a94a9cb3ea888ee89b88d63ed3
Ashish-u764 <123312898+Ashish-u764@users.noreply.github.com>
2024-08-03 19:17:33 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
+
 import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from './userSlice';
-
+//error
+//syntax error 
 const store = configureStore({
-    reducer={
+    reducer: {
         user: userReducer,
     }
 });
