3cc08730acfbb7cc4ed8e6735a27724d3fbf98cd
Ashish-u764 <123312898+Ashish-u764@users.noreply.github.com>
2024-08-03 19:18:50 +0530

    Update helperFunctions.js

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -2,6 +2,9 @@ export const timeAgo = (date) => {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     
     const today = new Date();
+    //error
+    //reviewDate not found
+    const reviewDate = new Date(date);
     const timeDifference = today - reviewDate;
 
     if (timeDifference < 60000) {
