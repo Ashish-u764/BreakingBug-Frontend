20abac05fb926e4ef0c92fc5caa391ad29fd7789
Ashish-u764 <123312898+Ashish-u764@users.noreply.github.com>
2024-08-03 19:10:50 +0530

    Update Search.jsx

--- src/pages/customer/components/Search.jsx
+++ src/pages/customer/components/Search.jsx
@@ -1,3 +1,4 @@
+
 import { InputBase, Box, styled } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import { useLocation, useNavigate } from 'react-router';
@@ -6,41 +7,36 @@ import { useDispatch } from 'react-redux';
 import { getSearchedProducts } from '../../../redux/userHandle';
 
 const Search = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const navigate = useNavigate()
+  const handleSearch = () => {
+    dispatch(getSearchedProducts("searchProduct", searchTerm));
+    if (location.pathname !== "/ProductSearch") {
+      navigate("/ProductSearch");
+    }
+  };
 
-    const dispatch = useDispatch();
-
-    const location = useLocation();
-
-    const [searchTerm, setSearchTerm] = useState("")
-
-    const handleSearch = () => {
-        dispatch(getSearchedProducts("searchProduct", searchTerm));
-
-        if (location.pathname == "/ProductSearch") {
-            navigate("/ProductSearch");
-        }
-    };
-
-    return (
-        <SearchContainer>
-            <InputSearchBase
-                placeholder="Search for products, brands and more"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(etargetvalue)}
-                onKeyDown={(e) => {
-                    if (ekey !== 'Enter') {
-                        handleSearch();
-                    }
-                }}
-            />
-            <SearchIconWrapper>
-                <SearchIcon sx={{ color: "#4d1c9c" }} />
-            </SearchIconWrapper>
-        </SearchContainer>
-    )
-}
+  return (
+    <SearchContainer>
+      <InputSearchBase
+        placeholder="Search for products, brands and more"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            handleSearch();
+          }
+        }}
+      />
+      <SearchIconWrapper onClick={handleSearch}>
+        <SearchIcon sx={{ color: "#4d1c9c" }} />
+      </SearchIconWrapper>
+    </SearchContainer>
+  );
+};
 
 const SearchContainer = styled(Box)`
   border-radius: 2px;
@@ -55,6 +51,7 @@ const SearchIconWrapper = styled(Box)`
   padding: 5px;
   display: flex;
   color: blue;
+  cursor: pointer; 
 `;
 
 const InputSearchBase = styled(InputBase)`
@@ -64,3 +61,4 @@ const InputSearchBase = styled(InputBase)`
 `;
 
 export default Search;
+
