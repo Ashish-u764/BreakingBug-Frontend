8d01d219cee9371d3a4a65ef51d3996a0f2b1705
Ashish-u764 <123312898+Ashish-u764@users.noreply.github.com>
2024-08-03 19:11:42 +0530

    Update CustomerOrders.jsx

--- src/pages/customer/pages/CustomerOrders.jsx
+++ src/pages/customer/pages/CustomerOrders.jsx
@@ -111,7 +111,9 @@ const CustomerOrders = () => {
                             </Stack>
 
                             <Grid container spacing={3}>
-                                {specificProductData && specificProductData.map((product, index) => (
+                            {/* //error 
+                            //use  responseSpecificProducts  */}
+                                {responseSpecificProducts && responseSpecificProducts.map((product, index) => (
                                     <Grid key={index} xs={12} sm={6} md={3}>
                                         <ProductCard product={product} />
                                     </Grid>
