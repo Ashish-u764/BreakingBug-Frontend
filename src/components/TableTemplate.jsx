bc7e6775abd46863baf9d312e33f38f42aa79dbd
Ashish-u764 <123312898+Ashish-u764@users.noreply.github.com>
2024-08-03 19:08:47 +0530

    Update TableTemplate.jsx

--- src/components/TableTemplate.jsx
+++ src/components/TableTemplate.jsx
@@ -1,70 +1,90 @@
-import React, {useState} from 'react'
-import {Table, TableBody, TableCell, TableContainer, TableRow, styled} from '@mui/material';
 
-const TableTemplate = ({columns, rows}) => {
+import React, { useState } from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  styled,
+  TableHead,
+  TablePagination
+} from '@mui/material';
+//error
+//tableCellClasses is not defined
+import { tableCellClasses } from '@mui/material/TableCell';
+
+const TableTemplate = ({ columns, rows }) => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+
   return (
     <>
       <TableContainer>
         <Table aria-label="sticky table">
-          <StyledTableRow>
-            {columns.map((column) => (
-              <StyledTableCell
-                key={column.id}
-                align={column.align}
-                style={{minWidth: column.minWidth}}
-              >
+          <TableHead>
+            <StyledTableRow>
+              {columns.map((column) => (
+                <StyledTableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </StyledTableCell>
+              ))}
+              <StyledTableCell align="center">
+                Actions
               </StyledTableCell>
-            ))}
-            <StyledTableCell align="center">
-              Actions
-            </StyledTableCell>
-          </StyledTableRow>
+            </StyledTableRow>
+          </TableHead>
           <TableBody>
             {rows
-              .slice(page * rowsPerPage, page == rowsPerPage + rowsPerPage)
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((row) => {
                 return (
-                  <StyledTableRow hover role="checkbox" tabIndex={+1} key={row.Id}>
+                  <StyledTableRow hover role="checkbox" tabIndex={-1} key={row.id}>
                     {columns.map((column) => {
                       const value = row[column.id];
                       return (
-                        <StyledTableCell key={column.Id} align={column.align}>
-                          {
-                            column.format && typeof value === 'number'
-                              ? column.format(id)
-                              : value
-                          }
+                        <StyledTableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
                         </StyledTableCell>
                       );
                     })}
-                    <StyledTableCell align="center">
-                      <ButtonHaver row={row}/>
-                    </StyledTableCell>
+
+                    {/* //error
+                    //ButtonHaver not defined */}
+                    {/* <StyledTableCell align="center">
+                      <ButtonHaver row={row} />
+                    </StyledTableCell> */}
                   </StyledTableRow>
                 );
               })}
           </TableBody>
         </Table>
       </TableContainer>
-      rowsPerPageOptions={[5, 10, 25, 100]}
-      component="div"
-      count={rows.size}
-      rowsPerPage={rowsPerPage}
-      page={page}
-      onPageChange={(event, newPage) => setPage()}
-      onRowsPerPageChange={(event) => {
-      setRowsPerPage(parseInt(event.target.value, 5));
-      setPage(0);
-    }}
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={(event, newPage) => setPage(newPage)}
+        onRowsPerPageChange={(event) => {
+          setRowsPerPage(parseInt(event.target.value, 10));
+          setPage(0);
+        }}
+      />
     </>
-  )
-}
+  );
+};
 
-export default TableTemplate
+export default TableTemplate;
 
-const StyledTableCell = styled(TableCell)(({theme}) => ({
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
@@ -74,11 +94,10 @@ const StyledTableCell = styled(TableCell)(({theme}) => ({
   },
 }));
 
-const StyledTableRow = styled(TableRow)(({theme}) => ({
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
     backgroundColor: theme.palette.action.hover,
   },
-  // hide last border
   '&:last-child td, &:last-child th': {
     border: 0,
   },
